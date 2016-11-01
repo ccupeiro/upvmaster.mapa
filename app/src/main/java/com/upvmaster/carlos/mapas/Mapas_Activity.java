14eae62a7598c6e1c7a8a789d7326f99d2fd11e3
Carlos Cupeiro <carlos.cupeiro@grupocmc.es>
2016-11-01 22:22:06 +0100

    Cambios MAPAS

--- app/src/main/java/com/upvmaster/carlos/mapas/Mapas_Activity.java
+++ app/src/main/java/com/upvmaster/carlos/mapas/Mapas_Activity.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.support.design.widget.Snackbar;
@@ -15,8 +16,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,12 +29,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Mapas_Activity extends FragmentActivity implements OnMapReadyCallback {
+import static com.upvmaster.carlos.mapas.R.id.mapa;
+
+public class Mapas_Activity extends FragmentActivity implements OnMapReadyCallback,LocationListener {
 
     private View vista;
     private GoogleMap mMap;
+    private double latitude,longitude;
     private final LatLng MADRID = new LatLng(39.481106, -0.340987);
-    private LatLng posicion;
+    protected LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +46,67 @@ public class Mapas_Activity extends FragmentActivity implements OnMapReadyCallba
         setContentView(R.layout.activity_mapas);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.mapa);
+                .findFragmentById(mapa);
         mapFragment.getMapAsync(this);
         vista = findViewById(R.id.vista_mapa);
         Button btn_colocar = (Button) findViewById(R.id.btn_colocar);
         btn_colocar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                animateCamera();
+            public void onClick(View v) {
+                Log.d("HOLA","HOLA");
             }
         });
-
-
+        iniciarGPS();
 
     }
 
+    private void iniciarGPS(){
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            //Que el GPS esté activo
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+                Criteria cri=new Criteria();
+                provider=locationManager.getBestProvider(cri,false);
+                if(provider!=null & !provider.equals("")){
+                    Location location=locationManager.getLastKnownLocation(provider);
+                    locationManager.requestLocationUpdates(provider, 400, 1, this);
+                    if(location!=null){
+                        onLocationChanged(location);
+                    }else{
+                        Toast.makeText(getApplicationContext(),"location not found",Toast.LENGTH_LONG ).show();
+                    }
+                }else{
+                    mostrarDialogoReinicio();
+                }
+
+            }else{
+                mostrarDialogoReinicio();
+            }
+
+        }
+    }
+
+    /* Request updates at startup */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
+
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.removeUpdates(this);
+        }
+
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -61,68 +114,37 @@ public class Mapas_Activity extends FragmentActivity implements OnMapReadyCallba
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setZoomControlsEnabled(false);
             mMap.getUiSettings().setCompassEnabled(true);
-            new LocalizacionTask().execute();
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(MADRID,15));
+            mMap.addMarker(new MarkerOptions().position(mMap.getCameraPosition().target));
         }
 
     }
 
-    public LatLng getPos() {
-        if (mMap.getMyLocation() != null) {
-            return new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
-        } else {
-            return MADRID;
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        if(mMap!=null){
+            LatLng posicion = new LatLng(location.getLatitude(),location.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(posicion));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posicion,15));
+            mMap.addMarker(new MarkerOptions().position(mMap.getCameraPosition().target));
         }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
-    public void animateCamera() {
-        if (mMap.getMyLocation() != null)
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude()), 15));
+    @Override
+    public void onProviderEnabled(String provider) {
+
     }
 
+    @Override
+    public void onProviderDisabled(String provider) {
 
-    private class LocalizacionTask extends AsyncTask<Void,Void,Void>{
-        ProgressDialog pd;
-
-        @Override
-        protected void onPreExecute() {
-            pd = new ProgressDialog(vista.getContext());
-            pd.setMessage(getString(R.string.pd_message));
-            pd.show();
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            LocationManager locationManager = (LocationManager)
-                    getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            posicion = null;
-            if (ActivityCompat.checkSelfPermission(Mapas_Activity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                Location location = locationManager.getLastKnownLocation(locationManager
-                        .getBestProvider(criteria, false));
-                if(location!=null){
-                    double latitude = location.getLatitude();
-                    double longitude = location.getLongitude();
-                    posicion = new LatLng(latitude,longitude);
-                }
-
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            if(posicion!=null){
-                //Buscar la localización
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posicion, 15));
-                mMap.addMarker(new MarkerOptions().position(mMap.getCameraPosition().target));
-                animateCamera();
-            }else{
-                //Dialogo
-                mostrarDialogoReinicio();
-            }
-            pd.dismiss();
-        }
     }
 
     //Dialogo Reinicio
@@ -133,7 +155,7 @@ public class Mapas_Activity extends FragmentActivity implements OnMapReadyCallba
         builder.setPositiveButton(R.string.dialog_reset, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // User clicked OK button
-                new LocalizacionTask().execute();
+                iniciarGPS();
             }
         });
         builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
